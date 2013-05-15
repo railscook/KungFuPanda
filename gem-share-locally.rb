b6441b93bdf30c41388dd395bec9be533eed41db
swe <swe@lovethis.com>
2013-05-15 05:59:00 +0100

    gem share among manchines

--- gem-share-locally.rb
+++ gem-share-locally.rb
@@ -1,3 +1,5 @@
+bundle show xxxx
+
 # local server
 gem server
 
@@ -8,19 +10,18 @@ source 'http://xxx.xxx.xx.xx:8808'
 rvm info
 #  "/home/your-username/.rvm/gems/ruby-1.9.2-p180@project-name/bundler/gems/amazing_crazy_gems-3df0f8b7ed10"
 pwd = Dir.pwd
-folders =  Dir['*/*.gemspec']
+folders =  Dir['precious*/*.gemspec']
 folders.each {|f|
  folder, file = f.split('/')
  path = "#{pwd}/#{folder}"
  Dir.chdir(path)
-
+ 
  system "gem build #{file}"
  puts folder; puts file; 
 
- current_file, version = file.split('.')
- system("cp #{file} #{current_file}.gem")
+ gem_file = Dir['*-*.gem'].first
  
- system "gem inabox ./#{current_file}.gem"
+ system "gem inabox ./#{gem_file}"
 }
 Dir.chdir(pwd)
 
